90bdaf943232aaf9b24f80aca6669c761e9556ef
TheStan <anton-dyubin@yandex.ru>
2024-05-19 16:12:49 +0500

    Update gpu_4.sh

--- gpu_4.sh
+++ gpu_4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 npm install
 
-
+--api tonapi --bin ./pow-miner-cuda --givers 10000
 while true; do
-  node send_universal_gpu.js --api tonapi --bin ./pow-miner-cuda --givers 10000 --gpu-count 4
+  node send_multigpu_gpu.js --api tonapi --bin ./pow-miner-cuda --givers 10000 --gpu-count 4
   sleep 1;
 done;
